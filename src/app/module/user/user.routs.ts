c13a4c98691b854be01c4da639eff9a0fc771e84
sibom007 <vaispark400@gmail.com>
2024-08-31 10:28:54 +0600

    al add

--- src/app/module/user/user.routs.ts
+++ src/app/module/user/user.routs.ts
@@ -1,32 +1,9 @@
 import express from 'express';
 import { UserControllers } from './user.contorler';
-import { userValidation } from './user.zodvalidation';
-import validateRequest from '../../middlewares/validateRequest';
-import auth from '../../middlewares/auth';
-import { Role } from '@prisma/client';
-
-
+import validateRequest from "../../middlewares/validateRequest";
 const router = express.Router();
 
-router.post(
-  '/register',
-  validateRequest(userValidation.createUser),
-  UserControllers.createUser
-);
-router.get(
-  '/donor-list',
-  UserControllers.getdonorUser
-);
-router.put(
-  '/my-profile',
-  auth(Role.USER, Role.ADMIN),
-  UserControllers.updateUserProfile
-);
-router.get(
-  '/my-profile',
-  auth(Role.USER, Role.ADMIN),
-  UserControllers.getUserProfile
-);
+router.post("/register", UserControllers.createUser);
 
 
 export const UserRoutes = router;
